df8313d8046496ebc9e12896d338c84c3db3b8ae
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-16 01:38:03 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song 
+  attr_accessor :name 
+
+  @@songs = []
+
+  def initialize(name)
+    @name = name 
+    save
+  end
+
+  def save
+    @@songs << self 
+  end 
+
+  def all 
+    @@songs 
+  end 
+
+end 
